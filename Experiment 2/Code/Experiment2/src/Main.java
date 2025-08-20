d1d63befc9d1bad98ff80f926546a9553a5acd0f
Manjot Singh <129639545+ManjotSingh2k05@users.noreply.github.com>
2025-08-20 11:22:53 +0530

    Add files via upload

---
+++ Experiment 2/Code/Experiment2/src/Main.java
@@ -0,0 +1,25 @@
+class Product {
+    int id;
+    String name;
+    double price;
+
+    Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    void displayDetails() {
+        System.out.println("Product Details:");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Product p1 = new Product(101, "Laptop", 75000);
+        p1.displayDetails();
+    }
+}
